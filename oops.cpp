14e468ceccf0439263f0828711b1a009a3ab503e
Ankit <ankitajnavi@gmail.com>
2022-06-25 17:02:01 +0530

    OOPs started

---
+++ oops.cpp
@@ -0,0 +1,38 @@
+/*
+Why OOPS?
+
+
+-> C++ language was designed with the main intention of adding object-oriented features to C language.
+
+-> As the size of the program increases, readability, maintainability and bug-free nature of programs decreases.
+
+-> This was the major problem with languages like C which relied upon functions or procedures (hence the name procedural oriented programming laguage)
+
+->As a result, the possibility of not addressing the problem in an effective manner was high.
+
+-> Also, as data was almost negelected, data security was wasily compromised.
+
+-> Using classes solves this problem by modelling program as a real world scenario.
+*/
+
+/*
+Basic Concept in Object Oriented Programmng ---->
+
+~> Classes --> (basic tmplate for creating objects) A class is an entity that determines how an object will behave and ehat the object will contain.
+    For example --> lets say we have a class Car which has data members(variables) such as speed, weight, price and function such as gearChange(), slowDown(), brake(), etc.
+
+~> Objects --> (basic run time entities) An object can be defined as an instance of a class, and there can be multiple instance of a class in a program.
+
+~> Data Abstraction & Encapsulation --> Wrapping data and function int single unit.
+
+~> Inheritance --> Properties of one class can be inherited into others.(For example :- Child inherits the traits of his/her parents)
+
+~> Polymorphism --> Ability to take more than one forms.
+    for example :- A man can many things Father, writer, student etc.
+    Same person can have different roll
+    // (Gangadhar hi Shaktimaan hai aur Shaktimaan hi Gangadhar hai...)
+
+Dynamic Binding --> Code which will execute is not known until the program runs.
+
+Message Passing --> Object.message(information) //call format
+*/
